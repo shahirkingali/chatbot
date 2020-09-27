9269b318e7e4fd862f201b4266282a9b76c9b0b8
Shahir Ali <shahirsheikali@gmail.com>
2020-09-26 23:38:20 -0700

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -16,5 +16,7 @@ elif "busy" in lifestyle:
     print("You should get Model 3")
 elif "slow" in lifestyle:
     print("You should get a Model Y or Model X ")
+elif "chill" in lifestyle:
+    print("Model Y, Model S")
 else:
     print("Sorry, but a Tesla is not for you :(")
