c80889fa298a4752811e070b4133735170f6e10c
Shahir Ali <shahirsheikali@gmail.com>
2020-09-26 23:36:42 -0700

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -7,3 +7,14 @@ print("Let's get started!\n")
 user = input("What is your name?\n")
 
 bot = print("Nice to meet you", user)
+
+	
+lifestyle = input("What type of lifestyle do you have: fast, busy, slow, or chill?")
+if "fast" in lifestyle:
+    print("You should get a Model 3 or Model S")
+elif "busy" in lifestyle:
+    print("You should get Model 3")
+elif "slow" in lifestyle:
+    print("You should get ")
+else:
+    print("Sorry, but a Tesla is not for you :(")
