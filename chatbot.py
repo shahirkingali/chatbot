55b7d22e02c220b58a3b045742ba6e4123c9e3ad
Shahir Ali <shahirsheikali@gmail.com>
2020-09-26 23:30:23 -0700

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,9 @@
+print("Hi, I am Maiya, Shahir's Assistant\n")
+
+print("I will help you choose the right Tesla model for you!\n")
+
+print("Let's get started!\n")
+
+user = input("What is your name?\n")
+
+bot = print("Nice to meet you", user)
