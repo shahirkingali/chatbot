0b0b492c80a6843162208429c924273354bccc01
Shahir Ali <shahirsheikali@gmail.com>
2020-09-30 14:23:54 -0700

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,25 +1,35 @@
 from random import choice
 
+
 def get_bot_response(user_response):
     print("I will help you choose the right Tesla model for you!\n")
+    fast_response = ["Model 3", "Model S"]
+    slow_response = ["Model Y", "Model X"]
+    busy_response = ["Model 3", "Model Y"]
+    chill_response = ["Model Y", "Model S"]
+    if "fast" == user_response:
+        return choice(fast_response)
+    elif "busy" == user_response:
+        return choice(busy_response)
+    elif "slow" == user_response:
+        return choice(slow_response)
+    elif "chill" == user_response:
+        return choice(chill_response)
+    else:
+        return "Sorry, but a Tesla is not for you :(!"
 
-print("Let's get started!\n")
+    print("Let's get started!\n")
+
 
 user = input("What is your name?\n")
 
 bot = print("Nice to meet you", user)
 
-	
-lifestyle = input("What type of lifestyle do you have: fast, busy, slow, or chill?")
-if "fast" in lifestyle:
-    print("You should get a Model 3 or Model S")
-elif "busy" in lifestyle:
-    print("You should get Model 3")
-elif "slow" in lifestyle:
-    print("You should get a Model Y or Model X ")
-elif "chill" in lifestyle:
-    print("Model Y, Model S")
-else:
-    print("Sorry, but a Tesla is not for you :(!")
+while True:
+    life_style = input(
+        "What type of lifestyle do you have: fast, busy, slow, or chill?")
+    results = get_bot_response(life_style)
+    print(results)
+    if life_style == "done":
+        break
 
-exit()
